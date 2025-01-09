6aa78f8394fc0837c690e317701e11c60e9fd7e8
Tanaka <keeneyes1114@gmail.com>
2025-01-09 11:45:05 -0800

    update

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended', // Integrates Prettier with ESLint
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    plugins: ['react', 'prettier'], // Includes the Prettier plugin
+    rules: {
+        'prettier/prettier': ['error'], // Ensures Prettier errors are treated as ESLint errors
+        'react/react-in-jsx-scope': 'off', // For Next.js, React is always in scope
+    },
+};
